0f699f45dd394f9f592e9c072247c1a3c829a0a3
Joel B. <smol@toast.cyou>
2023-12-13 11:22:40 -0500

    New template

--- src/infer.rs
+++ src/infer.rs
@@ -1,18 +1,19 @@
-use pyke_diffusers::{
-    OrtEnvironment, EulerDiscreteScheduler, StableDiffusionOptions, StableDiffusionPipeline, SchedulerOptimizedDefaults,
-    StableDiffusionTxt2ImgOptions
-};
-use std::sync::Arc;
+// use std::sync::Arc;
 
-pub fn infer(prompt: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let environment = Arc::new(OrtEnvironment::builder().build()?);
-    let mut scheduler = EulerDiscreteScheduler::stable_diffusion_v1_optimized_default()?;
-    let pipeline = StableDiffusionPipeline::new(&environment, "./stable-diffusion-v1-5", StableDiffusionOptions::default())?;
-    let mut imgs = StableDiffusionTxt2ImgOptions::default()
-        .with_prompt("photo of a red fox")
-        .with_steps(20)
-        .run(&pipeline, &mut scheduler)?;
+// use pyke_diffusers::{
+//     EulerDiscreteScheduler, OrtEnvironment, SchedulerOptimizedDefaults, StableDiffusionOptions, StableDiffusionPipeline,
+//     StableDiffusionTxt2ImgOptions
+// };
 
-    imgs[0].clone().into_rgb8().save("/tmp/result.png")?;
-    Ok(())
-}
+// pub fn infer(prompt: &str) -> Result<(), Box<dyn std::error::Error>> {
+//     let environment = Arc::new(OrtEnvironment::builder().build()?);
+//     let mut scheduler = EulerDiscreteScheduler::stable_diffusion_v1_optimized_default()?;
+//     let pipeline = StableDiffusionPipeline::new(&environment, "./stable-diffusion-v1-5", StableDiffusionOptions::default())?;
+//     let mut imgs = StableDiffusionTxt2ImgOptions::default()
+//         .with_prompt("photo of a red fox")
+//         .with_steps(20)
+//         .run(&pipeline, &mut scheduler)?;
+
+//     imgs[0].clone().into_rgb8().save("/tmp/result.png")?;
+//     Ok(())
+// }
