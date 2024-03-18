bed92851b541f683a23d8cd5d9b14b5ebb0e6443
Joel B. <smol@toast.cyou>
2024-03-18 15:34:36 -0400

    new readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# groucho
+
+![An application for generating AI images using Stable Diffusion](resources/screenshot.png)
+
+A frontend for Huggingface's `candle` library, written in GTK for Linux desktop clients. 
+
+## How it works
+
+Everything is local. Generating your first image will automatically download the selected Stable Diffusion version to the `~/.cache/huggingface/hub/` folder. 
+
+## Building
+
+`nix run .` will build the project. 
+
+Non-Nix systems will need the GTK depedencies. 
--- flake.nix
+++ flake.nix
@@ -11,20 +11,9 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rustVersion = pkgs.rust-bin.stable.latest.default;
-
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
-        };
-
-        myRustBuild = rustPlatform.buildRustPackage {
-          pname =
-            "groucho"; # make this what ever your cargo.toml package.name is
-          version = "0.1.0";
-          src = ./.; # the folder with the cargo.toml
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs =
+        buildInputs =
               with pkgs; [
+                openssl.dev
                 rustc
                 cargo
                 cairo
@@ -35,7 +24,6 @@
                 gtksourceview5
                 libadwaita
                 hicolor-icon-theme
-                openssl
                 pandoc
                 pango
                 pkg-config
@@ -48,14 +36,39 @@
                 git
                 wrapGAppsHook4
               ];
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        myRustBuild = rustPlatform.buildRustPackage {
+          pname =
+            "groucho"; # make this what ever your cargo.toml package.name is
+          version = "0.1.0";
+          src = ./.; # the folder with the cargo.toml
+          nativeBuildInputs = buildInputs;
+          
           cargoLock.lockFile = ./Cargo.lock;
         };
 
       in {
         defaultPackage = myRustBuild;
         devShell = pkgs.mkShell {
+          nativeBuildInputs = buildInputs;
           buildInputs =
             [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
           };
+
+        installPhase = ''
+          cp ${./resources/groucho.png} $out/share/pixmaps/myapp.png 
+          cp ${./resources/groucho.desktop} $out/share/groucho/groucho.desktop
+        '';
+
+        meta = with nixpkgs.lib; {
+          description = "groucho";
+          license = licenses.gpl3;
+          platforms = platforms.all;
+        };
       });
 }
